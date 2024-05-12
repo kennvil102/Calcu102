bd8d612813175f907ea668f42cdee21d57d66476
kennvil02 <kennvilchez@gmail.com>
2024-05-11 23:26:33 -0600

    update4

--- funcionesCalcu.js
+++ funcionesCalcu.js
@@ -1,17 +1,21 @@
-/* Primeros creamos las variables pantalla y botonsito*/ 
+//Primeros creamos las variables pantalla y botonsito
 
 /*Queryselector selecciona al primer elemento del dom que tiene la clase .display
 QuerySelectorAll selecciona a todos los elementos del dom con la clase .btn */  
 let pantalla=document.querySelector(".display")
 let botonsito=document.querySelectorAll(".btn")
 
+/* Botonsito tiene todos los botones le aplicamos el metodo foreach
+para aplicar la funcion a cada boton (foreach "cada uno") luego lo siguiente es aplicar 
+un listener a boton que contiene todos los botones hacemos otra funcion para que el boton al dar click extraiga el texto con textcontent */
 botonsito.forEach(boton=> {
 boton.addEventListener('click', ()=>{
 let botonApretado = boton.textContent;
-
+/*Hacemos una validacion si al apretar el (boton.id) y este es igual a c
+el contenido de la pantalla va ser igual a cero*/
 if (boton.id === "C") {
-    pantalla.textContent ="0";
-    return;    
+    pantalla.textContent ="0"; 
+    return;   
 }
 
 if (boton.id ==="delet") {
@@ -45,7 +49,7 @@ pantalla.textContent+= botonApretado;
 
 
 function Calcular() {
-    /*Primero hacemos las variables para obtener los datos caja1,caja2
+    /*Primero hacemos una funcion luego creamos las variables para obtener los datos caja1,caja2
     agregamos .value para obtener el valor que digite el usuario 
     */ 
     
@@ -58,6 +62,7 @@ function Calcular() {
     */
     let porcentaje = porcentajePropina / 100;
     let resultado1 = totalDeCompra * porcentaje;
+
     document.getElementById('resultad1').innerHTML = resultado1
     }
 
